cd711e95f986207bf292d70af5043a706a7ded9f
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-06-14 18:31:26 -0300

    feat: validates post schema and saves url in database

--- Repositories/postsRepository.js
+++ Repositories/postsRepository.js
@@ -2,16 +2,23 @@ import connection from "../config/db.js";
 
 async function getPostInfoByHashtag(hashtagId){
   return connection.query(`
-  SELECT posts.*
-  FROM posts
-  JOIN "postsHashtags" as ph
-  ON ph."postId" = posts.id
-  WHERE ph."hashtagId" = $1;
+    SELECT posts.*
+    FROM posts
+    JOIN "postsHashtags" as ph
+    ON ph."postId" = posts.id
+    WHERE ph."hashtagId" = $1;
   `, [hashtagId])
 };
 
+async function postUserUrl(values){
+  return connection.query(`
+    INSERT INTO posts(link, description, "userId") VALUES ($1, $2, $3);
+  `, values);
+}
+
 const postsRepository = {
-  getPostInfoByHashtag
+  getPostInfoByHashtag,
+  postUserUrl
 };
 
 export default postsRepository;
--- Routers/pagesRouter.js
+++ Routers/pagesRouter.js
@@ -1,10 +1,15 @@
 import {Router} from "express";
+
 import validateToken from "../Middlewares/authValidator.js";
 import validateHashtag from "../Middlewares/validateHashtag.js";
-import { getPostByHashtag } from "../Controllers/pagesController.js";
+import { validateSchema } from "../Middlewares/validateSchema.js";
+import { getPostByHashtag, postUrl } from "../Controllers/pagesController.js";
+
+import postSchema from "../Schemas/postSchema.js";
 
 const pagesRouter = Router();
 
 pagesRouter.get('/hashtag/:hashtag', validateToken, validateHashtag, getPostByHashtag)
+pagesRouter.post('/posts', validateSchema(postSchema), postUrl);
 
 export default pagesRouter;
