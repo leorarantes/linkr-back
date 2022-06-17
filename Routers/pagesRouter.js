e1724c52e7781d313511a8a421d74ad1f924eac7
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-06-17 16:58:55 -0300

    feat: gets user posts

--- Routers/pagesRouter.js
+++ Routers/pagesRouter.js
@@ -2,9 +2,10 @@ import {Router} from "express";
 
 import validateToken from "../Middlewares/authValidator.js";
 import validateHashtag from "../Middlewares/validateHashtag.js";
+import validateUserId from "../Middlewares/userValidator.js";
 import { validateSchema } from "../Middlewares/validateSchema.js";
 import { validateDescription } from "../Middlewares/validateDescription.js";
-import { getPostByHashtag, getTrendingHashtags, postUrl, getAllPosts, updatePost, deletePost } from "../Controllers/pagesController.js";
+import { getPostByHashtag, getTrendingHashtags, postUrl, getAllPosts, updatePost, deletePost, getPostByUser } from "../Controllers/pagesController.js";
 
 import postSchema from "../Schemas/postSchema.js";
 import descriptionSchema from "../Schemas/descriptionSchema.js";
@@ -17,5 +18,6 @@ pagesRouter.post('/posts', validateToken, validateSchema(postSchema), postUrl);
 pagesRouter.put('/posts/:postId', validateToken, validateDescription(descriptionSchema), updatePost);
 pagesRouter.delete('/posts/:postId', validateToken, deletePost);
 pagesRouter.get('/hashtag/:hashtag', validateToken, validateHashtag, getPostByHashtag);
+pagesRouter.get('/posts/:userId', validateToken, getPostByUser);
 
 export default pagesRouter;
