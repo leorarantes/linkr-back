141a1e03f29b829cc11c16bca3cb750a81b1d46a
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-06-22 15:16:19 -0300

    fix: gets the countof new posts

--- Routers/pagesRouter.js
+++ Routers/pagesRouter.js
@@ -17,12 +17,12 @@ pagesRouter.get('/trending', getTrendingHashtags);
 pagesRouter.get('/hashtag/:hashtag', validateToken, validateHashtag, getPostByHashtag);
 pagesRouter.get('/posts/:userId', validateToken, validateUserId, getPostByUser);
 pagesRouter.get('/new-posts/:postId', validateToken, getNewPosts);
+pagesRouter.get('/comments/count/:postId', validateToken, getCommentsAmount);
+pagesRouter.get('/comments/:postId', validateToken, getComments);
 pagesRouter.post('/posts', validateToken, validateSchema(postSchema), postUrl);
 pagesRouter.post('/hashtag', validateToken, hashtagExists, postHashtag);
 pagesRouter.post('/hashtagsPosts', validateToken, getPostByHashtagName, postHashtagsPost);
 pagesRouter.put('/posts/:postId', validateToken, validateDescription(descriptionSchema), updatePost);
 pagesRouter.delete('/posts/:postId', validateToken, deletePost);
-pagesRouter.get('/comments/count/:postId', validateToken, getCommentsAmount);
-pagesRouter.get('/comments/:postId', validateToken, getComments)
 
 export default pagesRouter;
