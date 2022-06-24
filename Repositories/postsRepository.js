e0e065091be8fe633aee9b48ef7dd590fe448842
Rafael Longo <rafaelongo45@gmail.com>
2022-06-24 03:42:50 -0300

    Update postsRepository.js

--- Repositories/postsRepository.js
+++ Repositories/postsRepository.js
@@ -92,15 +92,15 @@ async function deletePost(postId) {
 };
 
 async function getUserPosts(userId){
-  /*return connection.query(`
+  return connection.query(`
   SELECT p.*, u.name, u."photoLink" 
   FROM posts AS p
   JOIN users AS u
   ON u.id = p."userId"
-  JOIN shares AS s
-  ON s."userId" = p."userId"
-  WHERE "userId" = $1 OR id = s."postId"
-  ORDER BY p."createdAt" DESC;*/
+  LEFT JOIN shares AS s
+  ON s."postId" = p."id"
+  WHERE p."userId" = $1 or s."userId" = $1
+  ORDER BY p."createdAt" DESC;
 
   return connection.query(`
     SELECT p.*, u.name, u."photoLink" 
